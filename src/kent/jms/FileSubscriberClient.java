8ff18e4dfaef2b01cb8e54f7e65d9df074371c47
Kent Richardson <tkentrich@gmail.com>
2017-10-29 15:17:55 -0600

    Allow multiple subscriptions based on subsFile (/etc/subscribers.conf)

--- src/kent/jms/FileSubscriberClient.java
+++ src/kent/jms/FileSubscriberClient.java
@@ -1,6 +1,10 @@
 package kent.jms;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import java.util.Arrays;
 
@@ -21,12 +25,18 @@ public class FileSubscriberClient {
 		boolean argsDone = false;
 		boolean verbose = false;
 		String[] topics = {};
-		String destination = "/tmp";
+		String defaultDestination = "/tmp";
+		String subsFile = "/etc/subscribers.conf";
 
 		// Parse Arguments
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].equals("-v")) {
 				verbose = true;
+			} else if (args[i].equals("-s")) {
+				i++;
+				if (i < args.length) {
+					subsFile = args[i];
+				}
 			} else {
 				argsDone = true;
 			}
@@ -40,16 +50,39 @@ public class FileSubscriberClient {
 		connection = connectionFactory.createConnection();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-		for (String topicName : topics) {
-			if (verbose) {
-				System.out.println("Subscribing to topic: " + topicName);
+		// Read in subs
+		// for (String topicName : topics) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(subsFile));
+			String subLine = br.readLine();
+			while (subLine != null) {
+				String[] info = subLine.split(":");
+				String topicName = "";
+				String destination = "";
+				if (info.length != 2) {
+					System.err.println("WARNING: Subscription " + subLine + " has no destination. Using default" + defaultDestination);
+					topicName = subLine;
+					destination = defaultDestination;
+				} else {
+					topicName = info[0];
+					destination = info[1];
+				}
+				if (verbose) {
+					System.out.println("Subscribing to topic \"" + topicName + "\" with destination: " + destination);
+				}
+				Topic topic = session.createTopic(topicName);
+	
+				MessageConsumer consumer = session.createConsumer(topic);
+				consumer.setMessageListener(new FileConsumerMessageListener(topicName, new File(destination)));
+				subLine = br.readLine();
 			}
-			Topic topic = session.createTopic(topicName);
 
-			MessageConsumer consumer = session.createConsumer(topic);
-			consumer.setMessageListener(new FileConsumerMessageListener(topicName, new File(destination)));
+			connection.start();
+		} catch (FileNotFoundException e) {
+			System.err.println("ERROR: Subscription Specification file " + subsFile + " not found!");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
-		connection.start();
 	}
 }
--- src/kent/jms/FileSubscriberDaemon.java
+++ src/kent/jms/FileSubscriberDaemon.java
@@ -1,6 +1,10 @@
 package kent.jms;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
@@ -17,22 +21,66 @@ public class FileSubscriberDaemon implements Daemon {
 
 	Connection connection;
 	String topicName;
-	String destination;
+	String defaultDestination;
+	String subsFile;
+	boolean verbose;
 
 	@Override
 	public void init(DaemonContext dc) throws DaemonInitException, Exception {
+		verbose = false;
 		topicName = "DefaultTopic";
-		destination = "/tmp";
-		for (String s : dc.getArguments()) {
-			topicName = s;
+		defaultDestination = "/tmp";
+		subsFile = "/etc/subscribers.conf";
+
+		boolean argsDone = false;
+		String[] args = dc.getArguments();
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-v")) {
+				verbose = true;
+			} else if (args[i].equals("-s")) {
+				i++;
+				if (i < args.length) {
+					subsFile = args[i];
+				}
+			} else {
+				argsDone = true;
+			}
 		}
+
 		connection = null;
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
 		connection = connectionFactory.createConnection();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		Topic topic = session.createTopic(topicName);
-		MessageConsumer consumer = session.createConsumer(topic);
-		consumer.setMessageListener(new FileConsumerMessageListener(topicName, new File(destination)));
+
+		// Read in subscriptions
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(subsFile));
+			String subLine = br.readLine();
+			while (subLine != null) {
+				String[] info = subLine.split(":");
+				String topicName = "";
+				String destination = "";
+				if (info.length != 2) {
+					System.err.println("WARNING: Subscription " + subLine + " has no specified destination. Using " + defaultDestination);
+					destination = defaultDestination;
+					topicName = subLine;
+				} else {
+					topicName = info[0];
+					destination = info[1];
+				}
+				if (verbose) {
+					System.out.println("Subscribing to topic " + topicName + " with destination: " + destination);
+				}
+				Topic topic = session.createTopic(topicName);
+				MessageConsumer consumer = session.createConsumer(topic);
+				consumer.setMessageListener(new FileConsumerMessageListener(topicName, new File(destination)));
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("ERROR: Subscription Specification file not found");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
